63932136942d9031a19bfee3d280e256611ca9db
delpache <assandehugues@gmail.com>
2023-01-10 21:34:24 +0100

    Suppression de l'attribut :class

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -41,7 +41,6 @@
           id="username"
           name="username"
           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          :class="{ 'inline-flex text-sm font-mono text-red-500': errors.username }"
           required autofocus
       >
       </Field>
@@ -60,7 +59,6 @@
           id="password"
           name="password"
           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          :class="{ 'inline-flex text-sm font-mono text-red-500': errors.password }"
           required
       >
       </Field>
