d37a4ae6ca5d796d9510d5aaec76c564d43e3128
delpache <assandehugues@gmail.com>
2023-01-10 21:17:19 +0100

    Correction de la boucle infinie en enlevant le / sur la redirection

--- src/router/auth.routes.js
+++ src/router/auth.routes.js
@@ -2,8 +2,8 @@ export default {
     path: '/auth',
     component: () => import('../components/layouts/AuthLayout.vue'),
     children: [
-        { path: '', redirect: '/login' },
-        { path: '/login', component: () => import('../views/auth/LoginView.vue') },
-        { path: '/register', component: () => import('../views/auth/RegisterView.vue') }
+        { path: '', redirect: 'login' },
+        { path: 'login', component: () => import('../views/auth/LoginView.vue') },
+        { path: 'register', component: () => import('../views/auth/RegisterView.vue') }
     ]
 }
